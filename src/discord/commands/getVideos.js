5db066ba6834ba152693a1aeebfa43a88c76ba1b
Tim Swierzewski <TJswierzewski@gmail.com>
2021-06-21 14:40:24 -0400

    add thumbnail to embed post

--- src/discord/commands/getVideos.js
+++ src/discord/commands/getVideos.js
@@ -5,9 +5,10 @@ import Video from '../../models/video';
 const getVideos = async (client, interaction) => {
   const videos = await Video.find();
   const embed = new MessageEmbed().setTitle('Guide');
-  videos.forEach(({ title, url }) => {
+  embed.setThumbnail(`http://img.youtube.com/vi/${videos[0].videoId}/0.jpg`);
+  videos.forEach(({ title, videoId }) => {
     embed.addField('Title', title);
-    embed.addField('URL', url);
+    embed.addField('URL', `https://www.youtube.com/watch?v=${videoId}`);
   });
 
   reply(client, interaction, embed);
