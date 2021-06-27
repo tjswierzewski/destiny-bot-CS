196d29b95232b74e9052b6542005037342f5f1c0
Tim Swierzewski <TJswierzewski@gmail.com>
2021-06-27 11:08:58 -0400

    Make Message object

---
+++ src/lib/Message.js
@@ -0,0 +1,47 @@
+class Message {
+  constructor() {
+    this.id = null;
+    this.channel_id = null;
+    this.guild_id = null;
+    this.author = null;
+    this.member = null;
+    this.content = null;
+    this.timestamp = null;
+    this.edited_timestamp = null;
+    this.tts = null;
+    this.mention_everyone = null;
+    this.mentions = null;
+    this.mention_roles = null;
+    this.mention_channels = null;
+    this.attachments = null;
+    this.embeds = null;
+    this.reactions = null;
+    this.nonce = null;
+    this.pinned = null;
+    this.webhook_id = null;
+    this.type = null;
+    this.activity = null;
+    this.application = null;
+    this.application_id = null;
+    this.message_reference = null;
+    this.flags = null;
+    this.stickers = null;
+    this.referenced_message = null;
+    this.interaction = null;
+    this.thread = null;
+    this.components = null;
+  }
+
+  apiMessage() {
+    return {
+      content: this.content,
+      tts: this.tts,
+      file: this.file,
+      embeds: this.embeds,
+      message_reference: this.message_reference,
+      components: this.components,
+    };
+  }
+}
+
+export default Message;
