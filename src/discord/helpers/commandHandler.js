b1c68eff5a410787a90446abed5ee9205fae0642
Tim Swierzewski <TJswierzewski@gmail.com>
2021-06-27 12:07:02 -0400

    make interaction response class

--- src/discord/helpers/commandHandler.js
+++ src/discord/helpers/commandHandler.js
@@ -1,13 +1,14 @@
+import InteractionResponse from '../../lib/InteractionResponse';
 import discordPayloadConverter from './discordPayloadConverter';
 
 const commandHandler = (data) => {
   switch (data.name) {
     case 'ping':
-      return { type: 4, data: { content: 'hello world' } };
+      return new InteractionResponse({ content: 'hello world' });
 
     case 'embed':
       const { user, age } = discordPayloadConverter(data.options);
-      return { type: 4, data: { embeds: [{ title: age, author: { name: user } }] } };
+      return new InteractionResponse({ embeds: [{ title: age, author: { name: user } }] });
 
     default:
       return null;
---
+++ src/lib/InteractionResponse.js
@@ -0,0 +1,8 @@
+class InteractionResponse {
+  constructor(response) {
+    this.type = 4;
+    this.data = response;
+  }
+}
+
+export default InteractionResponse;
